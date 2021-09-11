86164556df811dc3739664746010efa248a34d92
Isaac Villa <isaacvillaluna@gmail.com>
2021-09-11 16:35:19 -0500

    cambio de tamaño de pantalla

--- p04-reto/src/main.js
+++ p04-reto/src/main.js
@@ -8,8 +8,8 @@ const config = {
   // AUTO: busca primero WEBGL y si no está disponible
   // eligirá CANVAS
   
-  width: 454, //Ancho de pantalla del juego
-  height: 313, //Alto de pantalla del juego
+  width: 1000, //Ancho de pantalla del juego
+  height: 500, //Alto de pantalla del juego
   parent: "contenedor", //Nombre del id del elemento <div> en el index.html
   // se refiere a dónde se pondrá el canvas o lienzo
   pixelArt: true, //Diseño con pixeles definidos (no borrosos)
