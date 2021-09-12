1e7d17eb6f9c47a5e9d381762eae9928bda1f9cd
Jesús Alejandro Yahuitl Rodríguez <jesusaleyaro@hotmail.com>
2021-09-12 12:24:43 -0500

    cardBacks

--- p04-reto/src/scenes/Bootloader.js
+++ p04-reto/src/scenes/Bootloader.js
@@ -1,11 +1,13 @@
-class Bootloader extends Phaser.Scene {
+import Utils from "./Utils.js"
 
+export default class Bootloader extends Phaser.Scene {
 
   constructor(scene) {
     super({
       key: "Bootloader"
     });
-
+    this.cards = ["imagen1", "imagen2", "imagen3", "imagen4", "imagen5", "imagen6",
+      "imagen7", "imagen8", "imagen9", "imagen10", "imagen11", "imagen12"];
   }
 
   init() {
@@ -14,77 +16,72 @@ class Bootloader extends Phaser.Scene {
 
   preload() {
     this.load.path = "src/scenes/assets/";
-    this.load.image(["imagen1", "imagen1a", "imagen2", "imagen2a", "imagen3", "imagen3a", "imagen4",
-      "imagen4a", "imagen5", "imagen5a", "imagen6", "imagen6a", "imagen7", "imagen7a", "imagen8",
-      "imagen8a", "imagen9", "imagen9a", "imagen10", "imagen10a", "imagen11", "imagen11a",
-      "imagen12", "imagen12a", "escenario"]);
+    this.load.image(this.cards);
+    this.load.image('cardBack');
+    this.load.image('escenario');
+  }
+
+
+
+  setCards(cardsMap) {
+
+    let shuffledArrayCards = Utils.shuffleArray(this.cards);
+
+    const xFirstCardFirstRow = 65;
+    const xLastCardFisrtRow = 905;
+    const yFisrtCardFirstRow = 70;
+    const yFisrtCardLastRow = 360;
+    const xStepCard = 120;
+    const yStepCard = 145;
+
+    let cardIterator = 0;
+
+    for (let j = yFisrtCardFirstRow; j <= yFisrtCardLastRow; j += yStepCard) {
+      for (let i = xFirstCardFirstRow; i <= xLastCardFisrtRow; i += xStepCard) {
+        if (!cardsMap.get(shuffledArrayCards[cardIterator])) {
+          cardsMap.set(shuffledArrayCards[cardIterator], this.add.image(i, j, shuffledArrayCards[cardIterator]).setInteractive());
+          cardsMap.get(shuffledArrayCards[cardIterator]).setScale(0.4, 0.4);
+          cardsMap.set(shuffledArrayCards[cardIterator]+'Back', this.add.image(i, j, 'cardBack').setInteractive());
+          cardsMap.get(shuffledArrayCards[cardIterator]+'Back').setScale(0.4, 0.4);
+          cardIterator++;
+          if(cardIterator==12){
+            cardIterator=0;
+            shuffledArrayCards = Utils.shuffleArray(shuffledArrayCards);
+          }
+        } else {
+          cardsMap.set(shuffledArrayCards[cardIterator] + 'a', this.add.image(i, j, shuffledArrayCards[cardIterator]).setInteractive());
+          cardsMap.get(shuffledArrayCards[cardIterator] + 'a').setScale(0.4, 0.4);
+          cardsMap.set(shuffledArrayCards[cardIterator]+'aBack', this.add.image(i, j, 'cardBack').setInteractive());
+          cardsMap.get(shuffledArrayCards[cardIterator]+'aBack').setScale(0.4, 0.4);
+          cardIterator++;
+        }
+      }
+    }
+
   }
 
   create() {
 
-    //images
+    //bg
     this.escenario = this.add.image(500, 250, "escenario");
-    this.imagen1 = this.add.image(65, 65, "imagen1").setInteractive();
-    this.imagen1.setScale(0.4, 0.4);
-    this.imagen1.setTint('0x000000');
-    this.imagen1a = this.add.image(185, 70, "imagen1a").setInteractive();
-    this.imagen1a.setScale(0.4, 0.4);
-    this.imagen2 = this.add.image(305, 70, "imagen2").setInteractive();
-    this.imagen2.setScale(0.4, 0.4);
-    this.imagen2a = this.add.image(425, 70, "imagen2a").setInteractive();
-    this.imagen2a.setScale(0.4, 0.4);
-    this.imagen3 = this.add.image(545, 70, "imagen3").setInteractive();
-    this.imagen3.setScale(0.4, 0.4);
-    this.imagen3a = this.add.image(665, 70, "imagen3a").setInteractive();
-    this.imagen3a.setScale(0.4, 0.4);
-    this.imagen4 = this.add.image(785, 70, "imagen4").setInteractive();
-    this.imagen4.setScale(0.4, 0.4);
-    this.imagen4a = this.add.image(905, 70, "imagen4a").setInteractive();
-    this.imagen4a.setScale(0.4, 0.4);
-    this.imagen5 = this.add.image(65, 215, "imagen5").setInteractive();
-    this.imagen5.setScale(0.4, 0.4);
-    this.imagen5a = this.add.image(185, 215, "imagen5a").setInteractive();
-    this.imagen5a.setScale(0.4, 0.4);
-    this.imagen6 = this.add.image(305, 215, "imagen6").setInteractive();
-    this.imagen6.setScale(0.4, 0.4);
-    this.imagen6a = this.add.image(425, 215, "imagen6a").setInteractive();
-    this.imagen6a.setScale(0.4, 0.4);
-    this.imagen7 = this.add.image(545, 215, "imagen7").setInteractive();
-    this.imagen7.setScale(0.4, 0.4);
-    this.imagen7a = this.add.image(665, 215, "imagen7a").setInteractive();
-    this.imagen7a.setScale(0.4, 0.4);
-    this.imagen8 = this.add.image(785, 215, "imagen8").setInteractive();
-    this.imagen8.setScale(0.4, 0.4);
-    this.imagen8a = this.add.image(905, 215, "imagen8a").setInteractive();
-    this.imagen8a.setScale(0.4, 0.4);
-    this.imagen9 = this.add.image(65, 360, "imagen9").setInteractive();
-    this.imagen9.setScale(0.4, 0.4);
-    this.imagen9a = this.add.image(185, 360, "imagen9a").setInteractive();
-    this.imagen9a.setScale(0.4, 0.4);
-    this.imagen10 = this.add.image(305, 360, "imagen10").setInteractive();
-    this.imagen10.setScale(0.4, 0.4);
-    this.imagen10a = this.add.image(425, 360, "imagen10a").setInteractive();
-    this.imagen10a.setScale(0.4, 0.4);
-    this.imagen11 = this.add.image(545, 360, "imagen11").setInteractive();
-    this.imagen11.setScale(0.4, 0.4);
-    this.imagen11a = this.add.image(665, 360, "imagen11a").setInteractive();
-    this.imagen11a.setScale(0.4, 0.4);
-    this.imagen12 = this.add.image(785, 360, "imagen12").setInteractive();
-    this.imagen12.setScale(0.4, 0.4);
-    this.imagen12a = this.add.image(905, 360, "imagen12a").setInteractive();
-    this.imagen12a.setScale(0.4, 0.4);
+    //cards
+    this.mapCards = new Map();
+    this.setCards(this.mapCards);
 
+    // //cardBack
+    // this.cardBack = this.add.image(65,70, "cardBack");
+    // this.cardBack.setScale(0.4, 0.4);
 
-    //events
-    const events = Phaser.Input.Events;
+    // //events
+    // const events = Phaser.Input.Events;
 
-    this.imagen1.on(events.POINTER_DOWN, function () {
-      this.clearTint();
-    });
+    // this.imagen1.on(events.POINTER_DOWN, function () {
+    //   alert('vamo a voltear');
+    // });
 
-    this.imagen1.on(events.POINTER_MOVE, function () {
-      console.log('la vida');
-    });
+    // this.imagen1.on(events.POINTER_MOVE, function () {
+    //   console.log('la vida');
+    // });
   }
 
 
@@ -94,5 +91,3 @@ class Bootloader extends Phaser.Scene {
   }
 
 }
-
-export default Bootloader;
